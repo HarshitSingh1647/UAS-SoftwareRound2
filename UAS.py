f45b857ea7e4cf912ea2ed730ea6535b9da74567
Harshit Singh <harshitsingh1647@gmail.com>
2026-09-05 09:35:15 +0530

    Update UAS.py

--- UAS.py
+++ UAS.py
@@ -1,17 +1,21 @@
 import cv2
 import numpy as np
-
-
-image = cv2.imread("./photos/1.jpg")
-
+#hsv(260.47, 91.06%, 92.16%)
+#hsv(258.44, 87.23%, 92.16%)
+#, 30, 
+image = cv2.imread("./photos/4.jpg")
+blank=cv2.bitwise_not(np.zeros_like(image))
 l_black = np.array([0, 0, 0])
 u_black = np.array([40, 40, 40])
 obstacle_mask = cv2.inRange(image, l_black, u_black)
-print(obstacle_mask)
-
-obstacle_mask = cv2.bitwise_not(obstacle_mask)
-cv2.imshow("obstacle_mask", obstacle_mask)
+water_mask = cv2.inRange(image, np.array([200, 10,70 ]), np.array([250, 50, 120]))
+cv2.imshow("obstacle_mask", cv2.bitwise_not(obstacle_mask))
 cv2.waitKey(0)
-cv2.destroyAllWindows()
-cv2.imwrite("obstacle_mask.png", obstacle_mask)
+cv2.imshow("water_mask", cv2.bitwise_not(water_mask))
+cv2.waitKey(0)
 
+non_mask = cv2.bitwise_or(obstacle_mask,water_mask)
+cv2.imshow("non_mask", cv2.bitwise_not(non_mask))
+cv2.waitKey(0)
+
+cv2.imwrite("obstacle_mask.png", obstacle_mask)
