a0baa5acb7360f345576c3c9e2ec0c54beb4f7da
Harshit Singh <harshitsingh1647@gmail.com>
2026-09-05 09:39:50 +0530

    Update and rename UAS.py to non_traversable.py

--- non_traversable.py
+++ non_traversable.py
@@ -5,17 +5,12 @@ import numpy as np
 #, 30, 
 image = cv2.imread("./photos/4.jpg")
 blank=cv2.bitwise_not(np.zeros_like(image))
-l_black = np.array([0, 0, 0])
-u_black = np.array([40, 40, 40])
-obstacle_mask = cv2.inRange(image, l_black, u_black)
+obstacle_mask = cv2.inRange(image, np.array([0, 0, 0]), np.array([40, 40, 40]))
 water_mask = cv2.inRange(image, np.array([200, 10,70 ]), np.array([250, 50, 120]))
-cv2.imshow("obstacle_mask", cv2.bitwise_not(obstacle_mask))
-cv2.waitKey(0)
-cv2.imshow("water_mask", cv2.bitwise_not(water_mask))
-cv2.waitKey(0)
+
 
 non_mask = cv2.bitwise_or(obstacle_mask,water_mask)
 cv2.imshow("non_mask", cv2.bitwise_not(non_mask))
 cv2.waitKey(0)
 
-cv2.imwrite("obstacle_mask.png", obstacle_mask)
+cv2.imwrite("non_mask.png", cv2.bitwise_not(non_mask))
